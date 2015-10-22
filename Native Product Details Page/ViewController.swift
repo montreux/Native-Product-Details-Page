a1c6c420e3b160464b3d52524e6818eec629100a
John Holcroft <john.holcroft@montreux.co.uk>
2015-10-22 10:46:08 +0100

    Initial Commit

---
+++ Native Product Details Page/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Native Product Details Page
+//
+//  Created by John Holcroft on 22/10/2015.
+//  Copyright © 2015 Marks and Spencer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Native Product Details PageTests/Native_Product_Details_PageTests.swift
@@ -0,0 +1,36 @@
+//
+//  Native_Product_Details_PageTests.swift
+//  Native Product Details PageTests
+//
+//  Created by John Holcroft on 22/10/2015.
+//  Copyright © 2015 Marks and Spencer. All rights reserved.
+//
+
+import XCTest
+@testable import Native_Product_Details_Page
+
+class Native_Product_Details_PageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
